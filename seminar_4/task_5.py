5063c13d470cf7a9fd36c0a9de87722964a907df
Irina_Sharafutdinova <irinsharafutdinov@yandex.ru>
2022-10-02 10:27:26 +0300

    task no.5 from seminar no.4 is done and the txt files are added

--- seminar_4/task_5.py
+++ seminar_4/task_5.py
@@ -1,43 +1,40 @@
+"""Задача №5. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов."""
+
 import random
 
-# запись в файл
-def write_file(name,st):
+def write_in_file(name,st): # запись в файл
     with open(name, 'w') as data:
         data.write(st)
 
-# создание случайного числа от 0 до 100
-def rnd():
+def create_num(): # создание случайного числа от 0 до 100
     return random.randint(0,101)
 
-# создание коэффициентов многочлена
-def create_mn(k):
-    lst = [rnd() for i in range(k+1)]
+def create_mn(k): # создание коэффициентов многочлена
+    lst = [create_num() for i in range(k+1)]
     return lst
     
-# создание многочлена в виде строки 
-def create_str(sp):
+def create_str(sp): # создание многочлена в виде строки
     lst= sp[::-1]
-    wr = ''
+    line = ''
     if len(lst) < 1:
-        wr = 'x = 0'
+        line = 'x = 0'
     else:
         for i in range(len(lst)):
             if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                line += f'{lst[i]}x^{len(lst)-i-1}'
                 if lst[i+1] != 0 or lst[i+2] != 0:
-                    wr += ' + '
+                    line += ' + '
             elif i == len(lst) - 2 and lst[i] != 0:
-                wr += f'{lst[i]}x'
+                line += f'{lst[i]}x'
                 if lst[i+1] != 0 or lst[i+2] != 0:
-                    wr += ' + '
+                    line += ' + '
             elif i == len(lst) - 1 and lst[i] != 0:
-                wr += f'{lst[i]} = 0'
+                line += f'{lst[i]} = 0'
             elif i == len(lst) - 1 and lst[i] == 0:
-                wr += ' = 0'
-    return wr
+                line += ' = 0'
+    return line
 
-# получение степени многочлена
-def sq_mn(k):
+def sq_mn(k): # получение степени многочлена
     if 'x^' in k:
         i = k.find('^')
         num = int(k[i+1:])
@@ -47,50 +44,44 @@ def sq_mn(k):
         num = -1
     return num
 
-# получение коэффицента члена многочлена
-def k_mn(k):
+def k_mn(k): # получение коэффицента члена многочлена
     if 'x' in k:
         i = k.find('x')
         num = int(k[:i])
     return num
 
-# разбор многочлена и получение его коэффициентов
-def calc_mn(st):
+def calc_mn(st): # разбор многочлена и получение его коэффициентов
     st = st[0].replace(' ', '').split('=')
     st = st[0].split('+')
-    lst = []
+    list = []
     l = len(st)
     k = 0
     if sq_mn(st[-1]) == -1:
-        lst.append(int(st[-1]))
+        list.append(int(st[-1]))
         l -= 1
         k = 1
     i = 1 # степень
     ii = l-1 # индекс
     while ii >= 0:
         if sq_mn(st[ii]) != -1 and sq_mn(st[ii]) == i:
-            lst.append(k_mn(st[ii]))
+            list.append(k_mn(st[ii]))
             ii -= 1
             i += 1
         else:
-            lst.append(0)
+            list.append(0)
             i += 1
         
-    return lst
+    return list
     
-
-
 # создание двух файлов
-
 k1 = int(input("Введите натуральную степень для первого файла k = "))
 k2 = int(input("Введите натуральную степень для второго файла k = "))
 koef1 = create_mn(k1)
 koef2 = create_mn(k2)
-write_file("file_1.txt", create_str(koef1))
-write_file("file_2.txt", create_str(koef2))
+write_in_file("file_1.txt", create_str(koef1))
+write_in_file("file_2.txt", create_str(koef2))
 
 # нахождение суммы многочлена
-
 with open('file_1.txt', 'r') as data:
     st1 = data.readlines()
 with open('file_2.txt', 'r') as data:
@@ -111,7 +102,7 @@ else:
     mm = len(lst2)
     for i in range(ll,mm):
         lst_new.append(lst2[i])
-write_file("file_res.txt", create_str(lst_new))
+write_in_file("file_res.txt", create_str(lst_new))
 with open('file_res.txt', 'r') as data:
     st3 = data.readlines()
 print(f"Результирующий многочлен {st3}")
